9f96ce6b6a11d4ebff9d34585233239227255551
chenqk <campchan@163.com>
2017-07-07 15:27:11 +0800

    导入资源
    导入依赖包
    导入资源文件

--- APP/Android/IMapp/app/src/main/java/com/keihong/imapp/MainActivity.java
+++ APP/Android/IMapp/app/src/main/java/com/keihong/imapp/MainActivity.java
@@ -1,14 +1,22 @@
 package com.keihong.imapp;
 
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainActivity extends com.keihong.imapp.common.app.Activity {
 
 
     @BindView(R.id.txt_test)
     TextView mTestText;
+    @BindView(R.id.btn_submit)
+    Button mBtnSubmit;
+    @BindView(R.id.edt_search)
+    EditText mEdtSearch;
+
 
     @Override
     protected int getContentLayoutId() {
@@ -20,4 +28,18 @@ public class MainActivity extends com.keihong.imapp.common.app.Activity {
         super.initWidget();
         mTestText.setText("Test Hello");
     }
+
+    @Override
+    protected void initData() {
+        super.initData();
+
+
+    }
+
+    @OnClick(R.id.btn_submit)
+    void onSubmit() {
+
+    }
+
+
 }
